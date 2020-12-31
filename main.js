64b2f3865b5e9e5f1609186c07878da8d0340849
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2020-12-31 18:29:27 +0100

    move to an object

--- main.js
+++ main.js
@@ -1,35 +1,68 @@
-const VERSION = 'v1.15'
+const VERSION = 'v1.15.2'
 
-var goal = new Date,
-	isPapiezowa = false,
-	Elements = {
+var Elements = {
 		display: undefined,
 		clock: undefined,
 		audio: undefined,
 		ver: undefined
+	},
+	Papiezowa
+
+Papiezowa = {
+	reached: false,
+	goal: new Date,
+	_onEvents: [],
+	_offEvents: [],
+	_on() {
+		for (let e of this._onEvents) {
+			e()
+		}
+	},
+	_off() {
+		for (let e of this._offEvents) {
+			e()
+		}
+	},
+	tick() {
+		var now = new Date
+		pad = x => ("0" + parseInt(x)).substr(-2)
+
+		if (now > this.goal) this.goal.setDate(this.goal.getDate() + 1)
+
+		var remain = ((this.goal - now) / 1000),
+			h = parseInt((remain / 60 / 60) % 60),
+			m = parseInt((remain / 60) % 60),
+			s = pad(remain % 60)
+
+		if (h > 0) m = pad(m)
+
+		Elements.clock.textContent = `${pad(now.getHours())}:${pad(now.getMinutes())}`
+		Elements.display.textContent = this.reached ? `${s}` : `${h > 0 ? `${h}:` : ''}${m > 0 ? `${m}:` : ''}${s}`
+
+		if (remain > 86340 && !this.reached) {
+			this.reached = true
+			this._on()
+		} else if (this.reached) {
+			this.reached = false
+			this._after()
+		}
+		setTimeout(() => {
+			this.tick()
+		}, 100)
+	},
+	addOn(e) {
+		this._onEvents.push(e)
+	},
+	addAfter(e) {
+		this._offEvents.push(e)
 	}
+}
 
 function rand(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function papiezowa() {
-	Elements.audio.play()
-	Elements.audio.style.bottom = '5%'
-	Elements.clock.style.fontSize = '12vh'
-	Elements.display.style.fontSize = '3vh'
-	document.body.classList.add("rainbow");
-}
-
-function popapiezowej() {
-	Elements.clock.style.fontSize = '5vh'
-	Elements.display.style.fontSize = '10vh'
-	document.body.classList.remove("rainbow");
-}
-
 window.onload = function () {
-
-	goal.setHours(21, 37, 0)
 
 	Elements = {
 		display: document.querySelector('#display'),
@@ -38,41 +71,36 @@ window.onload = function () {
 		ver: document.querySelector('#ver')
 	}
 
-	Elements.audio.volume = 0.5
+	// default goal time
+	Papiezowa.goal.setHours(21, 37, 0)
+
+	// default events
+	Papiezowa.addOn(function () {
+		Elements.audio.play()
+		Elements.audio.style.bottom = '5%'
+		Elements.clock.style.fontSize = '12vh'
+		Elements.display.style.fontSize = '3vh'
+		document.body.classList.add("rainbow")
+	})
+
+	Papiezowa.addAfter(function () {
+		Elements.clock.style.fontSize = '5vh'
+		Elements.display.style.fontSize = '10vh'
+		document.body.classList.remove("rainbow")
+	})
+
+	// audio player settings
+	Elements.audio.volume = 0.75
 
 	Elements.audio.onpause = function () {
-		if (!isPapiezowa) Elements.audio.style.bottom = '-25vh'
+		if (!Papiezowa.reached) Elements.audio.style.bottom = '-25vh'
 	}
-
-	// Elements.clock tick, goal detection
-	setInterval(() => {
-		var now = new Date
-		pad = x => ("0" + parseInt(x)).substr(-2)
-
-		if (now > goal) goal.setDate(goal.getDate() + 1)
-
-		var remain = ((goal - now) / 1000),
-			h = parseInt((remain / 60 / 60) % 60),
-			m = pad((remain / 60) % 60),
-			s = pad(remain % 60)
-
-		Elements.clock.textContent = `${pad(now.getHours())}:${pad(now.getMinutes())}`
-		Elements.display.textContent = isPapiezowa ? `${s}` : `${h > 0 ? `${h}:` : ''}${m > 0 ? `${m}:` : ''}${s}`
-
-		if (remain > 86340) {
-			if (isPapiezowa) return
-			isPapiezowa = true
-			papiezowa()
-		} else {
-			if (!isPapiezowa) return
-			isPapiezowa = false
-			popapiezowej()
-		}
-
-	}, 100);
 
 	// add 'dev' marker
 	if (VERSION.endsWith('dev')) ver.classList.add('dev')
 	else Elements.ver.classList.add('stable')
 	Elements.ver.textContent = VERSION
+
+	// clock tick, goal detection
+	Papiezowa.tick()
 }
