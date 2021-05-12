63bb32fec94a80e5da03faedbd6f42a9e3338c2b
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2021-05-12 18:01:05 +0200

    ???

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -24,7 +24,7 @@ function Nav(props) {
         },
         pathname = location.pathname,
         title = get(props.titles, pathname.split('/')),
-        suffix = 'Iledopapiezowej.pl ⏳'
+        suffix = 'Ile do papieżowej? ⏳'
 
         document.title = title ? (title + ' | ' + suffix) : suffix
         ga.pageview(pathname)
