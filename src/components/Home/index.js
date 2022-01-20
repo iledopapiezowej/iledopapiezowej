186b2a11b6dfd47f18146eeed6acda9cd6767a36
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2022-01-20 17:22:58 +0100

    fix: dont render turned off chat

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,108 +2,99 @@ import React, { useContext, useState } from 'react'
 
 import Counter from '../Counter'
 import Eyes from '../Eyes'
-import Player from '../Player';
-import Chat from '../Chat';
-import Promo from '../Promo';
+import Player from '../Player'
+import Chat from '../Chat'
+import Promo from '../Promo'
 
 import pkg from '../../../package.json'
 
 import GaContext from '../../contexts/Ga'
 import SettingsContext from '../../contexts/Settings'
 
-import './style.css';
+import './style.css'
 
 // fetch and update promo properties
 
 var promo = { hidden: true }
 
 fetch('/promo.json')
-  .then(data => {
-    return data.json()
-
-  })
-  .then(json => {
-    promo = {
-      hidden: false,
-      ...promo,
-      ...json
-    }
-
-  })
+	.then((data) => {
+		return data.json()
+	})
+	.then((json) => {
+		promo = {
+			hidden: false,
+			...promo,
+			...json,
+		}
+	})
 
 function Home(props) {
+	let ga = useContext(GaContext),
+		settings = useContext(SettingsContext)
 
-  let ga = useContext(GaContext),
-    settings = useContext(SettingsContext)
+	const [event, setEvent] = useState(false),
+		[elapsed, setElapsed] = useState(0)
 
-  const [event, setEvent] = useState(false),
-    [elapsed, setElapsed] = useState(0)
+	function eventStart(elapsed) {
+		setEvent(true)
+		setElapsed(elapsed)
+		ga.event({
+			category: '2137',
+			action: 'Event Start',
+			label: `${elapsed}s late`,
+			value: elapsed,
+			nonInteraction: true,
+		})
+	}
 
-  function eventStart(elapsed) {
-    setEvent(true)
-    setElapsed(elapsed)
-    ga.event({
-      category: '2137',
-      action: 'Event Start',
-      label: `${elapsed}s late`,
-      value: elapsed,
-      nonInteraction: true
-    })
-  }
+	function eventEnd() {
+		setEvent(false)
+		ga.event({
+			category: '2137',
+			action: 'Event End',
+			nonInteraction: true,
+		})
+	}
 
-  function eventEnd() {
-    setEvent(false)
-    ga.event({
-      category: '2137',
-      action: 'Event End',
-      nonInteraction: true
-    })
-  }
+	return (
+		<div
+			className={[
+				event ? 'event' : '',
+				settings.values.rainbow ? 'rainbow' : '',
+			].join(' ')}
+		>
+			<Counter
+				doClock={settings.values.clock}
+				doDisplay={settings.values.display}
+				onEventStart={(elapsed) => {
+					eventStart(elapsed)
+				}}
+				onEventEnd={() => {
+					eventEnd()
+				}}
+				sync={props.sync}
+			/>
 
-  return (
-    <div
-      className={[
-        event ? 'event' : '',
-        settings.values.rainbow ? 'rainbow' : ''
-      ].join(' ')}>
+			<Player elapsed={elapsed} event={event} doMusic={settings.values.music} />
 
-      <Counter
-        doClock={settings.values.clock}
-        doDisplay={settings.values.display}
-        onEventStart={(elapsed) => { eventStart(elapsed) }}
-        onEventEnd={() => { eventEnd() }}
-        sync={props.sync}
-      />
+			<Eyes
+				count={props.count}
+				invisible={props.invisible}
+				show={settings.values.eyes}
+			/>
 
-      <Player
-        elapsed={elapsed}
-        event={event}
-        doMusic={settings.values.music}
-      />
+			{settings.values.chat && (
+				<Chat socket={props.socket} show={settings.values.chat} />
+			)}
 
-      <Eyes
-        count={props.count}
-        invisible={props.invisible}
-        show={settings.values.eyes}
-      />
+			<Promo {...promo} closedDefault={settings.values['promo-' + promo.id]} />
 
-      <Chat
-        socket={props.socket}
-        show={settings.values.chat}
-      />
+			<div className="clear"></div>
 
-      <Promo
-        {...promo}
-        closedDefault={settings.values['promo-' + promo.id]}
-      />
-
-      <div className="clear"></div>
-
-      <span className="copyright">iledopapiezowej © 2020 v{pkg.version}</span>
-
-    </div>
-  );
-
+			<span className="copyright">iledopapiezowej © 2020 v{pkg.version}</span>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
