6e1c4263af6570af7a187b8b1f59ab078ee8b362
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2022-04-02 17:51:43 +0200

    fix: temp disable 4fun

--- src/components/Fun/index.js
+++ src/components/Fun/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route, useMatch, Link } from 'react-router-dom'
+// import React from 'react'
+// import { Routes, Route, useMatch, Link } from 'react-router-dom'
 
 // import Loading from '../Loading'
 
